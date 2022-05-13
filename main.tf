72bf7a304b11741b25f97899feaf911e77afa145
Josh Addington <jraddington@hotmail.com>
2022-05-13 18:04:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_mssql_server" "example" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "thisIsKat11"
-  minimum_tls_version          = "1.2"
+  minimum_tls_version          = "1.0"
   public_network_access_enabled = "true"
   outbound_network_restriction_enabled = "false"
 
