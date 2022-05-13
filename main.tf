2dc7c6e9baa0d8d74f3ca4a043c0a32bd297ec5f
Josh Addington <jraddington@hotmail.com>
2022-05-13 13:09:59 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+#Terraform code to create a SQL Server
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.5.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+#Create a Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "database-rg"
+  location = "West Europe"
+}
+
+#Create a SQL Server
+resource "azurerm_mssql_server" "example" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat11"
+  minimum_tls_version          = "1.2"
+  public_network_access_enabled = "true"
+  outbound_network_restriction_enabled = "false"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
